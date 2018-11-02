9d730a701759f2e31ad808cad00a5d72fa16ebe5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-02 18:36:40 +0000

    Deploy tonyhhyip/hkoscon-2019 to github.com/tonyhhyip/hkoscon-2019.git:gh-pages

---
+++ 0c97637e3edec40cff53.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{168:function(e,n,t){"use strict";t.r(n);t(77);n.default={functional:!0,props:{event:{type:Object,required:!0}},render:function(e,n){var t=n.props,p=n.slots,r=n.children;return t.event.topic?e("nuxt-link",{props:{to:new URL(t.event.internal).pathname.replace("/event/","/2019/topic/")}},r):p().default[0]}}}}]);
+//# sourceMappingURL=0c97637e3edec40cff53.js.map
